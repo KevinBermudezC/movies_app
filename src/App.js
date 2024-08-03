5f6ae39f3b3080fcc16a0265ffc7838e6e39708a
KevinBermudezC <kevinbermudezc@gmail.com>
2024-08-02 22:42:30 -0500

    "Updated API_URL from http to https"

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import searchIcon from "./search.svg"
 import MovieCard from "./MovieCard";
 
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=e0c8dbb9'
+const API_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=e0c8dbb9'
 
 const App =() =>{
     const [movies, setMovies] = useState([]);
